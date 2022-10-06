4f73b991d2af32839eec1d6bd71b1b7b97217c1b
phucnguyen202 <phucnguyenrk@gmail.com>
2022-10-06 09:38:43 +0700

    phuc commit

---
+++ app/src/main/java/com/example/phuc_2050531200252/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.phuc_2050531200252;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
